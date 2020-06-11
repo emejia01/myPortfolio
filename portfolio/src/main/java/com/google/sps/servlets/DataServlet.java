00a2ed4287c22f14552f8293b8d8b0acb64bd6cd
Erik <erikmej01@gmail.com>
2020-06-11 08:02:38 +0000

    Update Servlet file to include comments and setup for authentication

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -19,9 +19,23 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-// add imports
+// Add imports
 import java.util.ArrayList;
+import java.util.List;
 import com.google.gson.Gson;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import com.google.sps.data.Comment;
+// DataStore Imports
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.SortDirection;
+import com.google.appengine.api.datastore.FetchOptions;
+
+
 
 /** Servlet that returns some example content. TODO: modify this file to handle comments data */
 @WebServlet("/data")
@@ -29,30 +43,70 @@ public class DataServlet extends HttpServlet {
 
     ArrayList<String> messages = new ArrayList<String>();
 
-  @Override
-  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
-    response.setContentType("text/html;");
+        // Query DataStore for all comments
+        Query query = new Query("Comment").addSort("timestamp", SortDirection.DESCENDING);
+        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+        PreparedQuery results = datastore.prepare(query);
 
-    Gson gson = new Gson();
-    String jsonString = gson.toJson(messages);
-    response.getWriter().println(jsonString);
-  }
+        List<Comment> comments = new ArrayList<>();
+
+        for (Entity entity : results.asIterable()) {
+            String user_name = (String) entity.getProperty("username");
+            String date = (String) entity.getProperty("date");
+            String comment_text = (String) entity.getProperty("text");
+
+            comments.add(new Comment(user_name, comment_text, date));
+        }
+        
+        Gson gson = new Gson();
+        String jsonString = gson.toJson(comments);
+
+        response.setContentType("application/json;");
+        response.getWriter().println(jsonString);
+
+    }
+
 
     @Override
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         String comment_text = getParameter(request, "text-input", "");
+        String user_name = getParameter(request, "fname", "");
 
         response.setContentType("text/html;");
         response.getWriter().println(comment_text);
         messages.add(comment_text);
+
+        SimpleDateFormat formatter = new SimpleDateFormat("MM/dd/yyyy");
+        Date date = new Date(System.currentTimeMillis());
+        String comment_date = formatter.format(date);
+
+        // Create comment entity
+        Entity commentEntity = new Entity("Comment");
+
+        // Enter properties into comment entity
+        commentEntity.setProperty("text", comment_text);
+        commentEntity.setProperty("date", comment_date);
+        commentEntity.setProperty("timestamp", date);
+        commentEntity.setProperty("username", user_name);
+
+        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+        datastore.put(commentEntity);
+
+        // Redirect back to home-page
+        response.sendRedirect("/index.html");
     }
 
-  private String getParameter(HttpServletRequest request, String name, String defaultValue) {
-    String value = request.getParameter(name);
-    if (value == null) {
-      return defaultValue;
+
+    private String getParameter(HttpServletRequest request, String name, String defaultValue) {
+        String value = request.getParameter(name);
+        if (value == null) {
+            return defaultValue;
+        }
+        return value;
     }
-    return value;
-  }
+
+
 }
