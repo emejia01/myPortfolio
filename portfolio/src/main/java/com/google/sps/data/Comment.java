625aed5c54c3a4eaa652498e6e404bfa9822b635
Erik <erikmej01@gmail.com>
2020-06-16 18:12:33 +0000

    Make class attributes final

--- portfolio/src/main/java/com/google/sps/data/Comment.java
+++ portfolio/src/main/java/com/google/sps/data/Comment.java
@@ -3,14 +3,15 @@ package com.google.sps.data;
 // Commment Class Encapsulation to store comment properties in single object
 public final class Comment {
 
-    private String user_name;
-    private String comment_text;
-    private String date;
+    private final String user_name;
+    private final String comment_text;
+    private final String date;
 
     public Comment(String user_name, String comment_text, String date) {
         this.user_name = user_name;
         this.comment_text = comment_text;
         this.date = date;
+
     }
 
     public String getUserName() {
@@ -19,6 +20,7 @@ public final class Comment {
 
     public String getCommentText() {
         return this.comment_text;
+        
     }
 
     public String getDate() {
